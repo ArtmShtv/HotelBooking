f5a95c6aa63d005bad5d1f6df1daa4a7225a0f60
Artem Shitov <artemshitov1985@gmail.com>
2026-05-11 21:17:19 +0400

    Fix: style

--- hotel/views.py
+++ hotel/views.py
@@ -5,7 +5,7 @@ from rest_framework import serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
 
